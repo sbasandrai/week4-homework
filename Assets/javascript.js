a1a7e9e09e1fbd561bc747eb5a9df1c72a6e95b0
Sachin <sachin.basandrai@mwphillips.co.uk>
2022-04-30 10:05:51 +0100

    timer started

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,21 +1,79 @@
 const startButton = document.getElementById("start-btn");
 const bannerSection = document.getElementById("main-border");
 
+const mainElement = document.getElementById("main");
+
 let questionIndex = 0;
 
-const options = ["yes", "no", "maybe"];
+// const options = ["yes", "no", "maybe"];
+
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("mainn");
+
+var secondsLeft = 60;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    // const getCat = function (obj) {};
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    } else if (questions[0].options[1] === "December 31st") {
+      secondsLeft -= 30;
+    }
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  alert("GAME OVER");
+  removeQuestion();
+  document.getElementById("feedback-form").remove();
+
+  //   timeEl.textContent = " ";
+  //   var imgEl = document.createElement("img");
+  //   imgEl.setAttribute("src", "images/image_1.jpg");
+  //   mainEl.appendChild(imgEl);
+}
 
 const questions = [
-  { text: "Question 1", options: ["yes", "no", "maybe"] },
+  {
+    text: "The day before yesterday I was 25. The next year I will be 28. This is true only one day in a year. What day is my Birthday? ",
+    options: ["December 31st", "October 12th", "April 10th"],
+  },
 
-  { text: "Question 2", options: ["yes", "no", "maybe"] },
+  {
+    text: "If the 3rd of March is a Tuesday, what day of the week is the 1st of July",
+    options: ["Monday", "Wednesday", "Friday"],
+  },
 
-  { text: "Question 3", options: ["yes", "no", "maybe"] },
+  {
+    text: "How many sides does an octagon have?",
+    options: ["Six", "Seven", "Eight"],
+  },
 
-  { text: "Question 4", options: ["yes", "no", "maybe"] },
+  {
+    text: "Which of these is a famous mathematician?",
+    options: ["Pythagoras", "Marie Curie", "George Washington"],
+  },
+
+  {
+    text: "What is the square root of 64?",
+    options: ["Four", "Three", "Eight"],
+  },
 ];
 
-const handleOptionClick = (event) => {
+function handleOptionClick(event) {
   const currenttarget = event.currentTarget;
   const target = event.target;
 
@@ -31,7 +89,7 @@ const handleOptionClick = (event) => {
     const answer = { question, value };
 
     // store answer in local storage
-    storeAnswerInLS();
+    storeInLS("feedbackResults", answer);
 
     // go to next question if not last question
     removeQuestion();
@@ -39,7 +97,6 @@ const handleOptionClick = (event) => {
     if (questionIndex < questions.length - 1) {
       // go to next quesetion if not the last question
       // increment question index by 1
-
       questionIndex += 1;
 
       renderQuestion();
@@ -51,14 +108,91 @@ const handleOptionClick = (event) => {
       renderForm();
     }
   }
+}
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  // get full name from input
+  const fullName = document.getElementById("full-name").value;
+
+  // validate
+  if (fullName) {
+    // if valid then store feedbackResults in LS
+    const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+
+    // build object with fullName and results
+    const result = {
+      fullName,
+      feedbackResults,
+    };
+
+    // push the results back to LS
+    storeInLS("allResults", result);
+
+    // clear feedbackResults
+    localStorage.removeItem("feedbackResults");
+
+    // remove form
+    document.getElementById("feedback-form").remove();
+
+    scoretable();
+  } else {
+    alert("Please enter full name!");
+  }
 };
 
 const renderResults = () => {
   console.log("render results");
 };
 
+const scoretable = () => {
+  const table = document.createElement("table");
+  const h2 = document.createElement("h2");
+  h2.textContent = "Top Scores";
+};
+
 const renderForm = () => {
   console.log("render form");
+  const section = document.createElement("section");
+  section.setAttribute("class", "feedback-form-section");
+  section.setAttribute("id", "feedback-form");
+
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "title");
+  h2.textContent = "Submit your feedback";
+
+  const form = document.createElement("form");
+
+  const inputDiv = document.createElement("div");
+  inputDiv.setAttribute("class", "form-control");
+
+  const input = document.createElement("input");
+  input.setAttribute("id", "full-name");
+  input.setAttribute("class", "form-input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Enter full name");
+
+  inputDiv.append(input);
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("class", "form-control");
+
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.setAttribute("class", "btn");
+  button.textContent = "Submit";
+
+  buttonDiv.append(button);
+
+  form.append(inputDiv, buttonDiv);
+
+  section.append(h2, form);
+
+  mainElement.append(section);
+
+  // add event listener for form submission
+  form.addEventListener("submit", handleFormSubmit);
 };
 
 const renderQuestion = () => {
@@ -110,25 +244,36 @@ const removeQuestion = () => {
 
 // get feedback from LS
 
-const storeAnswerInLS = (answer) => {
-  const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+const initialiseLocalStorage = () => {
+  // get feedbackResults from LS
+  const feedbackResultsFromLS = JSON.parse(
+    localStorage.getItem("feedbackResults")
+  );
+
+  const allResultsFromLS = JSON.parse(localStorage.getItem("allResults"));
+
+  if (!feedbackResultsFromLS) {
+    // if not exist set LS to have feedbackResults as an empty array
+    localStorage.setItem("feedbackResults", JSON.stringify([]));
+  }
+
+  if (!allResultsFromLS) {
+    // if not exist set LS to have feedbackResults as an empty array
+    localStorage.setItem("allResults", JSON.stringify([]));
+  }
 };
 
-feedbackResults.push(answer);
+const storeInLS = (key, value) => {
+  // get feedbackResults from LS
+  const arrayFromLS = JSON.parse(localStorage.getItem(key));
 
-localStorage.setItem("feedbackResults", JSON.stringify(feedbackResults));
-const initialiseLocalStorage = () => {};
+  // push answer in to array
+  arrayFromLS.push(value);
 
-const feedbackResultsFromLS = JSON.parse(
-  localStorage.getItem("feedbackResults")
-);
-localStorage.getItem("feedbackResults");
+  // set feedbackResults in LS
+  localStorage.setItem(key, JSON.stringify(arrayFromLS));
+};
 
-// if it already exists, then do nothing
-
-if (!feedbackResultsFromLS) {
-  localStorage.setItem(JSON.stringify([]));
-}
 // else set LS to have feedbackresults as an empty array
 
 const removeBanner = () => {
@@ -142,6 +287,7 @@ const handleStartButtonClick = () => {
   //remove banner section
   removeBanner();
   renderQuestion();
+  setTime();
 };
 
 startButton.addEventListener("click", handleStartButtonClick);
